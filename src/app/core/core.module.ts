9045ee41fab0cda7c674c38f4e1a9df4c90b1977
Ujjal Dey <ujjaldey@gmail.com>
2018-11-18 11:23:32 +0800

    End of chapter 23

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,6 +8,9 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { RecipeService } from '../recipes/recipe.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from '../shared/auth.interceptor';
+import { LoggingInterceptor } from '../shared/loggin.interceptor';
 
 @NgModule({
   imports: [
@@ -23,6 +26,16 @@ import { RecipeService } from '../recipes/recipe.service';
     HeaderComponent,
     HomeComponent
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService]
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptor,
+      multi: true
+    }]
 })
 export class CoreModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -13,8 +13,6 @@ export class DataStorageService {
   constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-
     // return this.httpClient.put('https://ng-recipe-shopping-list-ud01.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
     //   // observe: 'events'
     //   observe: 'body',
@@ -23,20 +21,16 @@ export class DataStorageService {
     // });
 
     const req = new HttpRequest('PUT', 'https://ng-recipe-shopping-list-ud01.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
-      reportProgress: true,
-      params: new HttpParams().set('auth', token)
+      reportProgress: true
     });
     return this.httpClient.request(req);
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-
     // this.httpClient.get<Recipe[]>('https://ng-recipe-shopping-list-ud01.firebaseio.com/recipes.json?auth=' + token)
     this.httpClient.get<Recipe[]>('https://ng-recipe-shopping-list-ud01.firebaseio.com/recipes.json', {
       observe: 'body',
-      responseType: 'json',
-      params: new HttpParams().set('auth', token)
+      responseType: 'json'
     })
       .pipe(map(
         (recipes) => {
